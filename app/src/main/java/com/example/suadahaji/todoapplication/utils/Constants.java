eb51ffe7d13b599f361697611320981f33edbd89
Suada Haji <suada.haji@andela.com>
2017-04-27 15:58:47 +0300

    Create Constans Class for static values

---
+++ app/src/main/java/com/example/suadahaji/todoapplication/utils/Constants.java
@@ -0,0 +1,14 @@
+package com.example.suadahaji.todoapplication.utils;
+
+/**
+ * Created by suadahaji
+ */
+
+public class Constants {
+
+    public static final String DB_NAME = "taskdb";
+    public static final int DB_VERSION = 1;
+    public static final String TABLE_NAME = "task_table";
+    public static final String TABLE_COLUMN_ID = "ID";
+    public static final String TABLE_COLUMN_NAME = "name";
+}
